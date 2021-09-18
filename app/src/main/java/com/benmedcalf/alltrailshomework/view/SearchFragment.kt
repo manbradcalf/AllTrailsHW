410ea726a0c5b6e55980245bf8a835b0106b3ede
manbradcalf <ben.medcalf@gmail.com>
2021-09-18 11:44:24 -0400

    add searchfragment

---
+++ app/src/main/java/com/benmedcalf/alltrailshomework/view/SearchFragment.kt
@@ -0,0 +1,17 @@
+package com.benmedcalf.alltrailshomework.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.benmedcalf.alltrailshomework.R
+class SearchFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+}
