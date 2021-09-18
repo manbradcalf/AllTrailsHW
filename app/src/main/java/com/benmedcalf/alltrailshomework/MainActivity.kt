144d0477199af69f8a5943c8d0391320627ad021
manbradcalf <ben.medcalf@gmail.com>
2021-09-18 11:44:04 -0400

    add mapviewmodel to mainactivity

--- app/src/main/java/com/benmedcalf/alltrailshomework/MainActivity.kt
+++ app/src/main/java/com/benmedcalf/alltrailshomework/MainActivity.kt
@@ -2,17 +2,19 @@ package com.benmedcalf.alltrailshomework
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.findFragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.benmedcalf.alltrailshomework.databinding.ActivityMainBinding
-import com.benmedcalf.alltrailshomework.view.MapResultsFragmentDirections
+import com.benmedcalf.alltrailshomework.viewmodel.MapViewModel
 import com.benmedcalf.alltrailshomework.viewmodel.SearchViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var searchViewModel: SearchViewModel
+    private lateinit var mapViewModel: MapViewModel
 
     //region LifeCycle Callbacks
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +24,11 @@ class MainActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.nav_host_fragment_content_main)
         searchViewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+        mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
 
         binding.toggleViewFab.setOnClickListener {
-            //TODO: How to make this toggle button aware which fragment is active
-            val placeId = "ChIJN1t_tDeuEmsRUsoyG83frY4"
-            val action =
-                MapResultsFragmentDirections.actionMapResultsFragmentToDetailFragment(placeId)
-            navController.navigate(action)
+            //TODO("How to make this toggle button aware which fragment is active")
+
         }
     }
     //endregion
