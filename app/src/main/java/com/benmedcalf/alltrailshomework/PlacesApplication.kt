cd90b46d18913f2ac397dbabc1f33f4b2f0b40e2
manbradcalf <ben.medcalf@gmail.com>
2021-09-18 20:07:19 -0400

    Updated Application class to use Hilt

--- app/src/main/java/com/benmedcalf/alltrailshomework/PlacesApplication.kt
+++ app/src/main/java/com/benmedcalf/alltrailshomework/PlacesApplication.kt
@@ -1,10 +1,7 @@
 package com.benmedcalf.alltrailshomework
 
 import android.app.Application
-import com.benmedcalf.alltrailshomework.model.PlacesRepository
-import com.benmedcalf.alltrailshomework.model.local.PlaceDatabase
+import dagger.hilt.android.HiltAndroidApp
 
-class PlacesApplication : Application() {
-    val database by lazy { PlaceDatabase.getDatabase(this) }
-    val repository by lazy { PlacesRepository(database.placeDao()) }
-}
+@HiltAndroidApp
+class PlacesApplication : Application()
