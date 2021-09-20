ca5aebd77d7017a162653805d2d0098f9b433c28
manbradcalf <ben.medcalf@gmail.com>
2021-09-20 13:50:17 -0400

    small random updates

--- app/src/main/java/com/benmedcalf/alltrailshomework/view/ListResultsFragment.kt
+++ app/src/main/java/com/benmedcalf/alltrailshomework/view/ListResultsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -49,7 +48,18 @@ class ListResultsFragment : Fragment(R.layout.fragment_item_list) {
             viewLifecycleOwner.lifecycle.whenCreated {
                 // get and subscribe to the UI state
                 listViewModel.uiState.collect { listUiState ->
-                    renderResults(listUiState.placesRepositoryResult?.value!!.results)
+                    when (listUiState) {
+                        is ListUIState.Success -> {
+                            renderResults(listUiState.placesRepositoryResult?.value!!.placeDetails)
+                        }
+                        is ListUIState.Error -> {
+                            TODO("implement list error state")
+                        }
+                        is ListUIState.Loading -> {
+                            TODO("Implement list loading state")
+                        }
+                    }
+
                 }
             }
         }
--- app/src/main/java/com/benmedcalf/alltrailshomework/view/SearchFragment.kt
+++ app/src/main/java/com/benmedcalf/alltrailshomework/view/SearchFragment.kt
@@ -5,16 +5,21 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.benmedcalf.alltrailshomework.R
 import com.benmedcalf.alltrailshomework.databinding.FragmentSearchBinding
+import com.benmedcalf.alltrailshomework.model.PlacesRepository
+import com.benmedcalf.alltrailshomework.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+    private val searchViewModel: SearchViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,11 +35,18 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.filterbtn.setOnClickListener {
-            Toast.makeText(
-                requireContext(),
-                "search is ${binding.searchBox.query}",
-                Toast.LENGTH_LONG
-            ).show()
+            // TODO("show filtering options")
+            viewLifecycleOwner.lifecycleScope.launch {
+                searchViewModel.filterSearchResults()
+            }
+        }
+
+        binding.searchBox.setOnSearchClickListener {
+            val params =
+                PlacesRepository.SearchParameters(name = binding.searchBox.query.toString())
+            viewLifecycleOwner.lifecycleScope.launch {
+                searchViewModel.updateSearchResults(params)
+            }
         }
     }
 }
