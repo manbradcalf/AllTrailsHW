6caca52693dce2d891c8267ec8431cb17db60295
manbradcalf <ben.medcalf@gmail.com>
2021-09-22 14:10:44 -0400

    detailsviewmodel update

--- app/src/main/java/com/benmedcalf/alltrailshomework/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/benmedcalf/alltrailshomework/viewmodel/DetailsViewModel.kt
@@ -1,15 +1,20 @@
 package com.benmedcalf.alltrailshomework.viewmodel
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.benmedcalf.alltrailshomework.model.PlacesRepository
+import com.benmedcalf.alltrailshomework.model.RepoSearchResults
 import com.benmedcalf.alltrailshomework.model.remote.GooglePlacesService
 import com.benmedcalf.alltrailshomework.model.remote.placeDetails.PlaceDetailsResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class DetailsViewModel : ViewModel() {
+@HiltViewModel
+class DetailsViewModel
+@Inject constructor(private val repository: PlacesRepository) : BaseViewModel<RepoSearchResults>() {
     val details: MutableLiveData<PlaceDetailsResponse> by lazy {
         MutableLiveData<PlaceDetailsResponse>()
     }
